023433a2140f55172ab164c60813f177d0f7cc66
Hyunjoo <hyunjoopark0208@gmail.com>
2021-11-27 13:36:18 +0100

    feat: DOM 예제 추가

--- 2021-nov-week1/domPractice.js
+++ 2021-nov-week1/domPractice.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = function () {
   const h1 = document.createElement("h1");
   const h1Text = document.createTextNode("h1: Hello");
   h1.appendChild(h1Text);
@@ -18,21 +18,40 @@ window.onload = function() {
   // So there is no requirement to remove the node from its parent node
   //  before appending it to some other node.
   document.body.appendChild(p1);
-  
+
   console.log("h1이 속해있는 document는? " + h1.ownerDocument);
-  console.log("h1의 ownerDocument는 document인가요? : " + (h1.ownerDocument === window.document));
-  console.log("h1과 p의 ownerDocument는 같다? : " + (h1.ownerDocument.ownerDocument === p1.ownerDocument.ownerDocument));
+  console.log(
+    "h1의 ownerDocument는 document인가요? : " +
+      (h1.ownerDocument === window.document)
+  );
+  console.log(
+    "h1과 p의 ownerDocument는 같다? : " +
+      (h1.ownerDocument.ownerDocument === p1.ownerDocument.ownerDocument)
+  );
 
   // NodeList
   const nodeList = document.querySelectorAll("p");
-  console.log("querySelectorAll은 NodeList를 리턴합니다: ", JSON.stringify(nodeList));
+  console.log(
+    "querySelectorAll은 NodeList를 리턴합니다: ",
+    JSON.stringify(nodeList)
+  );
   nodeList.item(0).style = "color: blue;";
   nodeList[1].style = "color: red;";
-  
+
   // Attr
   console.log("빨간 p의 id attr? ", nodeList[1].getAttribute("id"));
   const div1 = document.getElementById("div1");
   console.log("div1의 style은? ", div1.getAttribute("style"));
   const div1IdAttr = div1.getAttributeNode("id");
   console.log(div1IdAttr.value === "div1");
-}
+
+  // Interface
+  const table = document.getElementById("table");
+  console.log(table);
+  const tableAttrs = table.attributes; // Element interface
+  for (let i = 0; i < tableAttrs.length; i++) {
+    if (tableAttrs[i].nodeName.toLowerCase() === "border") {
+      table.border = "1"; // HTMLTableElement interface
+    }
+  }
+};
